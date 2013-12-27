d94824db99d22d5ab969716e7bf503ca790eaf4e
jrri <jrri@imm.dtu.dk>
2013-12-27 21:46:02 +0100

    scj: A debug PEH

---
+++ java/target/src/rtapi/test/level1/DebugPEH.java
@@ -0,0 +1,49 @@
+package test.level1;
+
+import javax.realtime.PeriodicParameters;
+import javax.realtime.PriorityParameters;
+import javax.safetycritical.ManagedMemory;
+import javax.safetycritical.Mission;
+import javax.safetycritical.PeriodicEventHandler;
+import javax.safetycritical.Services;
+import javax.safetycritical.StorageParameters;
+import javax.safetycritical.annotate.Level;
+import javax.safetycritical.annotate.Phase;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.SCJRestricted;
+
+public class DebugPEH extends PeriodicEventHandler{
+
+	public DebugPEH(PriorityParameters priority, PeriodicParameters release,
+			StorageParameters storage, long scopeSize, String name) {
+		super(priority, release, storage, scopeSize, name);
+	}
+	
+	@Override
+	@SCJAllowed
+	public void handleAsyncEvent() {
+		
+		System.out.println(getName());
+		
+		Mission.getCurrentMission().requestTermination();
+		
+	}
+	
+	@Override
+	@SCJAllowed(Level.SUPPORT)
+	@SCJRestricted(phase = Phase.CLEANUP)
+	public void cleanUp() {
+		
+		ManagedMemory.enterPrivateMemory(256, new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				System.out.println("cleanup in nested");
+				
+			}
+		});
+		
+	}
+
+}
